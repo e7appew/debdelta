f324f1f9754d04e4a28fcd5db730f5e87037d07b
A Mennucc <mennucc1@debian.org>
2007-04-14 09:26:07 +0200

    debdelta (0.20) unstable; urgency=low
      * added man pages. thanks  Pierre-yves Jégou (Closes: 376598)
      * fix bug in name/len file matching when building
        debdeltas (debdelta would die of "division by zero" sometimes)
    
     -- A Mennucc1 <mennucc1@debian.org>  Sat, 14 Apr 2007 09:26:07 +0200

--- debdelta/contrib/histograms.sh
+++ debdelta/contrib/histograms.sh
@@ -13,25 +13,58 @@ PREFIX="$1"
 
 shift
 
+t=`tempfile`
+
+echo ===== working towards $PREFIX
+
 dir=`dirname $0`
 
 awk -f $dir/sizes_histogram.awk "$@" > ${PREFIX}sizes 
+
+
+if test -s ${PREFIX}sizes ; then
+cat > $t <<EOF
+set term png
+set out "${PREFIX}sizes.png"
+set xlabel "% of patchsize / new deb size"
+set ylabel "% of deltas"
+plot "${PREFIX}sizes" w steps
+EOF
+gnuplot < $t
+fi
 
 
 sizes_by_instsize=`tempfile`
 awk '/^NEW/{P=$3 ; SI=$9 * 1 ; SZ = $16 * 1 ;};
 /^ deb delta is/{if (SI > 1 ) { SZ = $16 * 1; TOTDEBSZ = TOTDEBSZ + SZ ; PC = $4 * 1 ; \
   TOTDELTASZ = TOTDELTASZ + ( SZ * PC   ) ;   print SI, " ",  PC,SZ }} ;
-END{printf("#average=%d\n", TOTDELTASZ / TOTDEBSZ );}' "$@" > $sizes_by_instsize
+END{if(TOTDEBSZ>0){printf("#size average=%d\n", TOTDELTASZ / TOTDEBSZ );};}' "$@" > $sizes_by_instsize
 
 sizes_by_instsize_avg=`tail -1 $sizes_by_instsize | cut -d= -f2 `
+
+
+if grep average  $sizes_by_instsize ; then
+cat > $t <<EOF
+set term png
+set out "${PREFIX}sizes_by_size.png"
+set ylabel "% of patch size / new deb size"
+set xlabel "installed size of new deb (in kB)"
+set logscale x 2
+set arrow from 1600,${sizes_by_instsize_avg} to 3200,${sizes_by_instsize_avg} nohead  front
+set label "avg ${sizes_by_instsize_avg}" at  3200,${sizes_by_instsize_avg} front
+set title " (% of patch size / new deb size) by new.deb.size ; average=${sizes_by_instsize_avg}"
+plot "$sizes_by_instsize" w points
+EOF
+gnuplot < $t
+fi
+###
 
 delta_speeds_by_instsize=`tempfile`
 awk '/^NEW/{P=$3 ; SI=$9 * 1};
 /^ deb delta is/{SZ = $16 * 1 ; }
 /^ delta time/{ if( SI > 1) { N = N+1 ;  TOTSZ = TOTSZ + SZ ; TOTTIM = TOTTIM + $3 ; SP= $6 * 1 ; TSP=TSP+SP ; print SI, " ", SP }};
 #/^Total running time:/{TOTTIM = $4 * 1 }
-END{printf("#average=%d\n", TOTSZ / TOTTIM );}' "$@" >  $delta_speeds_by_instsize 
+END{if(TOTTIM>0){printf("#create average=%d\n", TOTSZ / TOTTIM )};}' "$@" >  $delta_speeds_by_instsize 
 
 delta_speeds_by_instsize_avg=`tail -1 $delta_speeds_by_instsize | cut -d= -f2`
 
@@ -39,25 +72,13 @@ patch_speeds_by_instsize=`tempfile`
 awk '/^NEW/{P=$3 ; SI=$9 * 1};
 /^ deb delta is/{SZ = $16 * 1 ;}
 / Patching done/{ if( SI > 1) {N = N+1 ; TOTSZ = TOTSZ + SZ ;  TOTTIM = TOTTIM + ($4 * 1 ); SP= $6 * 1 ; TSP=TSP+SP ; print SI, " ", SP }};
-END{printf("#average=%d\n", TOTSZ / TOTTIM ) ; }' "$@" > $patch_speeds_by_instsize
+END{if(TOTTIM>0){printf("#patch average=%d\n", TOTSZ / TOTTIM )} ; }' "$@" > $patch_speeds_by_instsize
 
 patch_speeds_by_instsize_avg=`tail -1 $patch_speeds_by_instsize | cut -d= -f2`
 
-t=`tempfile`
-
+if grep average  $delta_speeds_by_instsize && grep average  $patch_speeds_by_instsize ; then
 cat > $t <<EOF
 set term png
-set out "${PREFIX}sizes.png"
-set xlabel "% of patchsize / new deb size"
-set ylabel "% of deltas"
-plot "${PREFIX}sizes" w steps
-set out "${PREFIX}sizes_by_size.png"
-set ylabel "% of patch size / new deb size"
-set xlabel "installed size of new deb (in kB)"
-set logscale x 2
-set arrow from 1600,${sizes_by_instsize_avg} to 3200,${sizes_by_instsize_avg} nohead  front
-set label "avg ${sizes_by_instsize_avg}" at  3200,${sizes_by_instsize_avg} front
-plot "$sizes_by_instsize" w points
 set out "${PREFIX}speeds_by_size.png"
 unset arrow
 unset label
@@ -70,10 +91,12 @@ set arrow from 1600,${delta_speeds_by_instsize_avg} to 3200,${delta_speeds_by_in
 set label "create avg ${delta_speeds_by_instsize_avg}" at  5000,${delta_speeds_by_instsize_avg} front
 set arrow from 1600,${patch_speeds_by_instsize_avg} to 3200,${patch_speeds_by_instsize_avg} nohead  front
 set label "patch avg ${patch_speeds_by_instsize_avg}" at 3200,${patch_speeds_by_instsize_avg} front
+set title "speed by new.deb.size ; average when patching =${patch_speeds_by_instsize_avg} creating=${delta_speeds_by_instsize_avg}"
 plot "$delta_speeds_by_instsize" w points title "create" , "$patch_speeds_by_instsize" w points title "patch" 
 quit
 EOF
 
 gnuplot < $t
+fi
 
 rm $t $sizes_by_instsize $delta_speeds_by_instsize $patch_speeds_by_instsize
--- debdelta/contrib/sizes_histogram.awk
+++ debdelta/contrib/sizes_histogram.awk
@@ -2,7 +2,7 @@
 / deb delta is.*/{p = $4 * 1 ; s = $9 * 1 ;
 bucket = int(p / 5) ; count++;
 if (p <= 80 ) { cou ++; hist[ bucket ] ++ ;  cumul += p ; savin += s };};
-END{ print "# ",int(cou * 100 /count) ,"% of deltas are <=80% of original;"
+END{if(cou>0){ print "# ",int(cou * 100 /count) ,"% of deltas are <=80% of original;"
 print "# of those, the average percent is ",int(cumul / cou), "%"
 print "# of those, the average saving  is ",int(savin / cou), "kB"
 print "# and the histogram is"
@@ -14,6 +14,6 @@ while(val<=16) {
   printf("%2d %2d %s\n", val*5, l,substr("-------------------------------------------------------...",1,l) )
  } 
 val++
-}
+}}
 #for (val in  hist) print val*5,int(100*hist[val]/cou) 
 };
