c118f66ac62fe27b23e1e822cf8b01136e576b00
A Mennucc <mennucc1@debian.org>
2009-09-04 23:29:06 +0200

    debdelta (0.32) unstable; urgency=low
      * debdeltas: add support for Packages.gz and  Packages.bz2 .
      * debdeltas: add option --test  (that does what '-d' was doing before)
      * debdelta,debdeltas:
        - add option --disable-feature ,
        - and add document README.features explaining backward compatibility,
        - and correct some bugs in features handling.
      * Change Depends: lzma, xdelta, xdelta3, bsdiff to Recommends
         ('debpatch' and 'debdelta-upgrade' will ask if they are needed).
      * Fix exit status and document it in man page.
      * Fix -v and -d , and document '-d' better in man pages.
      * debpatch, debdelta-upgrade : if '-d' and a delta fails,
        create some files that may be used to address the bug.
      * Bug fix: "redownload package already in the cache", thanks to Sandro
        Tosi (Closes: #544784).
    
     -- A Mennucc1 <mennucc1@debian.org>  Fri, 04 Sep 2009 23:29:06 +0200

--- debdelta/debpatch.sh
+++ debdelta/debpatch.sh
@@ -12,47 +12,49 @@ if test "$3" = "" ; then
  exit
 fi
 
-
 if test "$2" = / ; then
  echo Sorry, for this simple script you really need the old deb 
  exit 1
 fi
 
-delta=`realpath $1`
-olddeb=`realpath $2`
+case "$1" in 
+    /*) delta="$1" ;;
+    *) delta=`pwd`/"$1" ;;
+esac
+olddeb="$2"
 newdeb="$3"
 
-
-
 TD=${TMPDIR:-/tmp}/debpatch$$
-mkdir $TD
+mkdir $TD $TD/OLD $TD/OLD/CONTROL $TD/OLD/DATA $TD/NEW $TD/NEW/CONTROL $TD/NEW/DATA $TD/PATCH
 
-pushd $TD > /dev/null
+ln -s '/usr/lib/debdelta/minigzip' '/usr/lib/debdelta/minibzip2' $TD/
 
-ln -s '/usr/lib/debdelta/minigzip' '/usr/lib/debdelta/minibzip2' .
+ar p $olddeb control.tar.gz  | tar -x -z -p -f - -C $TD/OLD/CONTROL
 
-mkdir OLD OLD/CONTROL OLD/DATA NEW NEW/CONTROL NEW/DATA PATCH
-
-ar p $olddeb control.tar.gz  | tar -x -z -p -f - -C OLD/CONTROL
-
-if ar t $olddeb | grep -q data.tar.bz2 ; then
- ar p $olddeb data.tar.bz2  | tar -x --bzip2 -p -f - -C OLD/DATA
+if ar t $olddeb | grep -q data.tar.lzma ; then
+ ar p $olddeb data.tar.lzma  | unlzma -c | tar -x -p -f - -C $TD/OLD/DATA
+elif ar t $olddeb | grep -q data.tar.bz2 ; then
+ ar p $olddeb data.tar.bz2  | tar -x --bzip2 -p -f - -C $TD/OLD/DATA
 else
- ar p $olddeb data.tar.gz  | tar -x -z -p -f - -C OLD/DATA
+ ar p $olddeb data.tar.gz  | tar -x -z -p -f - -C $TD/OLD/DATA
 fi
 
+pushd $TD > /dev/null
 
 cd PATCH
 ar x "$delta"
 cd $TD
 
-if test -r  PATCH/patch.sh.gz  ; then
+if test -r  PATCH/patch.sh.lzma  ; then
+ unlzma PATCH/patch.sh.lzma
+elif test -r  PATCH/patch.sh.gz  ; then
  gunzip PATCH/patch.sh.gz
 elif test -r  PATCH/patch.sh.bz2  ; then
  bunzip2 PATCH/patch.sh.bz2
 fi
 
-sh -e PATCH/patch.sh
+#dash will not work, see bug 379227
+bash -e PATCH/patch.sh
 
 #note that we do not check MD5 in this simple script..
 
